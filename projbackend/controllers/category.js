4bfb035b399bf05261c0c49f5ee3b8e77e80cd8e
Sahil Makvana <17bce053@nirmauni.ac.in>
2022-07-15 00:22:52 +0530

    Commit

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const category = require("../models/category");
 const Category = require("../models/category");
 
 exports.getCategoryId = (req, res, next, id) => {
@@ -52,5 +53,19 @@ exports.updateCategory = (req, res) => {
             });
         }
         res.json({ updatedCategory });
+    });
+};
+
+exports.removeCategory = (req, res) => {
+    const category = req.category;
+    category.remove((err, category) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Failed to delete this category",
+            });
+        }
+        res.json({
+            message: "Successfully deleted",
+        });
     });
 };
--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryId, createCategory, getCategory, getAllCategory, updateCategory } = require("../controllers/category");
+const { getCategoryId, createCategory, getCategory, getAllCategory, updateCategory, removeCategory } = require("../controllers/category");
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 const { getUserById, getUser } = require("../controllers/user");
 
@@ -20,5 +20,6 @@ router.get("/categories", getAllCategory);
 router.put("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, updateCategory);
 
 // Delete Routes
+router.delete("/category/:categoryId/:userId", isSignedIn, isAuthenticated, isAdmin, removeCategory);
 
 module.exports = router;
