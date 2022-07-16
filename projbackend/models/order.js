8b577fe59fac1214194d2017bc9c246b5a5b22d1
Sahil Makvana <17bce053@nirmauni.ac.in>
2022-07-16 17:59:20 +0530

    commit

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -4,38 +4,46 @@ const { ObjectId } = mongoose.Schema;
 const ProductCartSchema = new mongoose.Schema({
     product: {
         type: ObjectId,
-        ref: "Product"
+        ref: "Product",
     },
     name: {
-        type: String
+        type: String,
     },
     count: {
-        type: Number
-    }, 
+        type: Number,
+    },
     price: {
-        type: Number
-    }
-})
+        type: Number,
+    },
+});
 
 const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
 
-const orderSchema = new mongoose.Schema({
-    products: [ProductCartSchema],
-    transaction_id: {},
-    amount: {
-        type: Number
+const orderSchema = new mongoose.Schema(
+    {
+        products: [ProductCartSchema],
+        transaction_id: {},
+        amount: {
+            type: Number,
+        },
+        address: {
+            type: String,
+        },
+        ststus: {
+            type: String,
+            default: "Received",
+            enum: ["Cancelled", "Delivered", "Shipped", "Processing", "Received"],
+        },
+        updated: {
+            type: Date,
+        },
+        user: {
+            type: ObjectId,
+            ref: "User",
+        },
     },
-    address: {
-        type: String
-    },
-    updated: {
-        type: Date
-    },
-    user: {
-        type: ObjectId,
-        ref: "User"
-    }
-}, {timestamps: true})
+    { timestamps: true }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 module.exports = { Order, ProductCart };
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, createProduct, getProduct, photo } = require("../controllers/product");
+const { getProductById, createProduct, getProduct, photo, deleteProduct, updateProduct, getAllProducts, getAllUniqueCategories } = require("../controllers/product");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
@@ -13,5 +13,9 @@ router.param("productId", getProductById);
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
 router.get("/product/:productId", getProduct);
 router.get("/product/photo/:productId", photo);
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct);
+router.get("/products", getAllProducts);
+router.get("/products/categories", getAllUniqueCategories);
 
 module.exports = router;
