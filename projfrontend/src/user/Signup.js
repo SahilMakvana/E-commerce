574a097aa29d858f23b52ed725d7a1969305657f
Sahil Makvana <17bce053@nirmauni.ac.in>
2022-07-21 00:30:10 +0530

    Commit

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,37 +1,58 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
-
-const signUpForm = () => (
-    <div className="row">
-        <div className="col-md-6 offset-sm-3 text-left">
-            <form>
-                <div className="form-group">
-                    <label className="text-light">Name</label>
-                    <input className="form-control" type="text" />
-                </div>
-                <div className="form-group">
-                    <label className="text-light">Email</label>
-                    <input className="form-control" type="email" />
-                </div>
-                <div className="form-group">
-                    <label className="text-light">Password</label>
-                    <input className="form-control" type="password" />
-                </div>
-                <div className="form-group">
-                    <label className="text-light"> </label>
-                </div>
-                <div class="d-grid mx-auto">
-                    <button class="btn btn-success" type="button">
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-);
+import { signup } from "../auth/helper";
 
 const Signup = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false,
+    });
+
+    const { name, email, password, error, success } = values;
+
+    const handleChange = (name) => (event) => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setValues({ ...values, error: false });
+        signup({ name, email, password }).then();
+    };
+
+    const signUpForm = () => (
+        <div className="row">
+            <div className="col-md-6 offset-sm-3 text-left">
+                <form>
+                    <div className="form-group">
+                        <label className="text-light">Name</label>
+                        <input className="form-control" onChange={handleChange("name")} type="text" />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-light">Email</label>
+                        <input className="form-control" onChange={handleChange("email")} type="email" />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-light">Password</label>
+                        <input className="form-control" onChange={handleChange("password")} type="password" />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-light"> </label>
+                    </div>
+                    <div class="d-grid mx-auto">
+                        <button class="btn btn-success" type="button">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+
     return (
         <Base title="Signup Page" description="A page for user Signup">
             {signUpForm()}
